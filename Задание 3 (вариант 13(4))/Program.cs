2ce358814f3937200befb8343ee94584fc0c5b62
Admin <Admin@DESKTOP-82I2RIP>
2022-09-30 20:23:55 +0300

    Добавьте файлы проекта.

---
+++ Задание 3 (вариант 13(4))/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите расстояние в м :"); // Ввод растояния в метрах
+int i = int.Parse(Console.ReadLine()); //Объявление целочисленной переменной
+Console.Write("Полных км: ");
+Console.WriteLine(i / 1000); //Вывод числа полных киллометров 
